6a8de640bb62ad268a9e66ac7a927db43f5f0ddd
Peter H. Jin <peter@peterjin.org>
2021-07-26 10:48:49 -0500

    Add common promises

---
+++ common_promises.js
@@ -0,0 +1,36 @@
+function readFromSocket(socket) {
+	return new Promise((resolve, reject) => {
+		let handlers = {
+			data: function(buf) {
+				socket.removeListener('data', handlers.data);
+				socket.removeListener('error', handlers.error);
+				socket.removeListener('end', handlers.end);
+				resolve(buf);
+			},
+			error: function() {
+				socket.removeListener('data', handlers.data);
+				socket.removeListener('error', handlers.error);
+				socket.removeListener('end', handlers.end);
+				reject();
+			},
+			end: function() {
+				socket.removeListener('data', handlers.data);
+				socket.removeListener('error', handlers.error);
+				socket.removeListener('end', handlers.end);
+				resolve(null);
+			}
+		};
+		socket.on('data', handlers.data);
+		socket.on('error', handlers.error);
+		socket.on('end', handlers.end);
+	});
+}
+function socketConnect(options) {
+	return new Promise((resolve, reject) => {
+		let conn = net.createConnection(options);
+		conn.once('connect', () => resolve(conn));
+		conn.once('error', () => reject());
+	});
+}
+exports.readFromSocket = readFromSocket;
+exports.socketConnect = socketConnect;
