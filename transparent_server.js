df498a6c37556fc812b52fc064ead67628abd156
Peter H. Jin <peter@peterjin.org>
2021-07-26 12:38:37 -0500

    Set the type in transparent server

--- transparent_server.js
+++ transparent_server.js
@@ -1,4 +1,9 @@
 async function transparent_server(conn) {
-	return {host: conn.localAddress, port: conn.localPort, excessBuf: null};
+	let host = String(conn.localAddress);
+	let type = 'ipv4';
+	if (host.indexOf(':') >= 0) {
+		type = 'ipv6';
+	}
+	return {host: host, port: Number(conn.localPort), excessBuf: null, type: type};
 }
 exports.transparent_server = transparent_server;
