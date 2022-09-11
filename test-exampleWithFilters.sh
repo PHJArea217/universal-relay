f4723d094c52b63c839d09ef2643e739420fa8fc
Peter H. Jin <peter@peterjin.org>
2022-09-11 02:03:54 -0400

    test-exampleWithFilters.sh: Switch to testing example.js

--- test-exampleWithFilters.sh
+++ test-exampleWithFilters.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -eu
-exec "$CTRTOOL" ns_open_file \
+exec "$CTRTOOL" ns_open_file -o 9 \
 	-m -P "/proc/$1/ns/net" -O rdonly -O nonblock -O noctty -s0,i \
-	-n -6 ::,80,ato -l4096 -i 0,n -U \
-	-n -6 ::,443,ao -l4096 -i 0,n -U \
-	-n -6 ::,8080,ao -l4096 -i 0,n -U \
 	-n -d inet -4 127.0.0.10,81,a -l4096 -i 0,n -U \
-	node test.js
+	-n -6 ::,80,ato -l4096 -i 0,n -U \
+	-n -6 '::,1080,a' -l4096 -i 0,n -U \
+	node example.js
