cb0b51b5ee8f93122160d8f1907607a074113f85
Peter H. Jin <peter@peterjin.org>
2023-04-01 13:58:47 -0400

    start_dev2: Make more friendly for rootful operation

--- start_dev2.sh
+++ start_dev2.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eu
-unshare --fork -r -m -p --propagation=slave --mount-proc "${CTRTOOL:-ctrtool}" launcher -n --alloc-tty --script-is-shell --no-clear-groups --no-set-id --script='./test-exampleWithFilters.sh self/fd/"$2" "" X &' sh -eu -c '
+: "${CTRTOOL:=ctrtool}"
+export CTRTOOL
+unshare --fork -r -m -p --propagation=slave --mount-proc "$CTRTOOL" launcher -n --alloc-tty --script-is-shell -U --escape -m --uid-map=0.0.1 --gid-map=0.0.1 --no-clear-groups --script='./test-exampleWithFilters.sh self/fd/"$2" "" X &' sh -eu -c '
 ip link set lo up
 ip route add local "fedb:1200:4500:7800::/64" dev lo
 ip6tables-nft -t mangle -A PREROUTING -p tcp -d "fedb:1200:4500:7800::/64" -j TPROXY --on-ip "::" --on-port 80
