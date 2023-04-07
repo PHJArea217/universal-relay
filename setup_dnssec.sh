136533e3813cf73e424301bca9134aefca7afde1
Peter H. Jin <peter@peterjin.org>
2023-04-06 20:51:59 -0400

    Add setup_dnssec.sh

---
+++ setup_dnssec.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eu
+if [ ! -f /etc/powerdns/dnssec.db ]; then
+	sed -i '/^launch=remote$/claunch=gsqlite3,remote\ngsqlite3-dnssec=yes\ngsqlite3-database=/etc/powerdns/dnssec.db' /etc/powerdns/pdns.conf
+	sqlite3 /etc/powerdns/dnssec.db <"${1:-/usr/share/doc/pdns-backend-sqlite3/schema.sqlite3.sql}"
+	sqlite3 /etc/powerdns/dnssec.db 'INSERT INTO domains (name, type) VALUES (".", "NATIVE");'
+	pdnsutil secure-zone . || :
+	pdnsutil set-nsec3 '1 0 0 -' native
+	pdnsutil show-zone .
+else
+	printf 'DNSSEC already configured, run rm -f /etc/powerdns/dnssec.db to redo\n' >&2
+fi
