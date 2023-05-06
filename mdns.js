2525ebce3f80b5011f5b6cfba4a5f703c5f9dc3f
Peter H. Jin <peter@peterjin.org>
2023-05-06 18:55:16 -0400

    Add systemd resolve function

--- mdns.js
+++ mdns.js
@@ -1,6 +1,7 @@
 const common_promises = require('./common_promises.js');
+/* The implementations here were reverse engineered by strace-ing :) */
 async function mdns_resolve(hostname, simplePath, allow_linklocal_scope, orig_endpoint) {
-	let simpleConn = await common_promises.socketConnect(simplePath, null);
+	let simpleConn = await common_promises.socketConnect(simplePath || '/run/avahi-daemon/socket', null);
 	simpleConn.write(`RESOLVE-HOSTNAME ${hostname}\n`);
 	let response = [];
 	while (true) {
@@ -21,4 +22,39 @@ async function mdns_resolve(hostname, simplePath, allow_linklocal_scope, orig_en
 	}
 	throw new Error(`mdns_resolve: failed to resolve ${hostname}`);
 }
+async function systemd_resolve(hostname, simplePath, allow_linklocal_scope, orig_endpoint) {
+	let simpleConn = await common_promises.socketConnect(simplePath || '/run/systemd/resolve/io.systemd.Resolve', null);
+	let requestJSON = {method: "io.systemd.Resolve.ResolveHostname",parameters: {name: hostname, flags: 0}};
+	simpleConn.write(JSON.stringify(requestJSON));
+	simpleConn.write(new Buffer([0]));
+	let response = [];
+	let continueLoop = true;
+	while (continueLoop) {
+		let response_buffer = await common_promises.readFromSocket(simpleConn);
+		if (!response_buffer) break;
+		for (let c of response_buffer) {
+			if (c === 0) {
+				continueLoop = false;
+				break;
+			}
+			response.push(c);
+		}
+	}
+	simpleConn.destroy();
+	let result_buffer = Buffer.from(response).toString();
+	let match_result = JSON.parse(result_buffer);
+	// console.log(result_buffer);
+	if (match_result && match_result.parameters && Array.isArray(match_result.parameters.addresses)) {
+		let result = [];
+		for (let a of match_result.parameters.addresses) {
+			if ((a.family === 2) || (a.family === 10)) {
+				let ip_address = orig_endpoint.clone().setIPBuffer(Buffer.from(a.address));
+				result.push(ip_address);
+			}
+		}
+		return result;
+	}
+	throw new Error(`systemd_resolve: failed to resolve ${hostname}`);
+}
 exports.mdns_resolve = mdns_resolve;
+exports.systemd_resolve = systemd_resolve;
