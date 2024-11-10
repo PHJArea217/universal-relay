ce2cdff707ba73d4b583e20a601110bf3fc646e5
Peter H. Jin <peter@peterjin.org>
2024-11-09 20:17:19 -0500

    Oops

--- src/common_promises.js
+++ src/common_promises.js
@@ -6,4 +6,36 @@ exports.readFromSocket = async function (s) {
 	}
 	return null;
 }
+function socketConnect(options, destroyOnClose) {
+	return new Promise((resolve, reject) => {
+		let conn = net.createConnection(options);
+		let done = false;
+		conn.once('connect', () => {
+			if (done) return;
+			done = true;
+			conn.pause();
+			resolve(conn);
+		});
+		conn.once('error', () => {
+			if (done) return;
+			done = true;
+			conn.destroy();
+			reject();
+		});
+		setTimeout(() => {
+			if (done) return;
+			done = true;
+			conn.destroy();
+			reject();
+		}, 10000);
+		if (destroyOnClose) {
+			destroyOnClose.on('close', () => {
+				if (done) return;
+				done = true;
+				conn.destroy();
+				reject();
+			});
+		}
+	});
+}
 exports.socketConnect = socketConnect;
